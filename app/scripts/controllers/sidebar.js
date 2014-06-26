e682b3188c5dcebfeec0493081bb6f6f828fb833
taeho.cho <chohome2@gmail.com>
2014-06-26 22:24:41 +0900

    add all files

---
+++ app/scripts/controllers/sidebar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('mutzipAdminApp')
+  .controller('SidebarCtrl', function ($scope, Auth) {
+        initLTE();
+        $scope.user = Auth.user;
+        $scope.userRoles = Auth.userRoles;
+        $scope.accessLevels = Auth.accessLevels;
+  });
